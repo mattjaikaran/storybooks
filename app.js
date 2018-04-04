0401071fb504384ae02b59835db153337d4307d1
Matthew Jaikaran <mattjaikaran@gmail.com>
2018-04-03 22:57:00 -0400

    Started UI

--- app.js
+++ app.js
@@ -1,22 +1,55 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
 const passport = require('passport')
+
+// Load User Model
+require('./models/User')
 
 // Passport config
 require('./config/passport')(passport)
 
 // Load routes
+const index = require('./routes/index')
 const auth = require('./routes/auth')
+
+// Load keys
+const keys = require('./config/keys')
+
+// Mongoose connect
+mongoose.connect(keys.mongoURI)
+  .then(() => { console.log('MongoDB Connected') })
+  .catch(err => { console.log(err) })
 
 const app = express()
 
+// Handlebars Middleware
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}))
+app.set('view engine', 'handlebars')
 
-app.get('/', (req, res) => {
-  res.send('It works')
+app.use(cookieParser())
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false
+}))
+
+// Passport Middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
+// Set Global Vars
+app.use((req, res, next) => {
+  res.locals.user = req.user || null
+  next()
 })
 
-
 // Use routes
+app.use('/', index)
 app.use('/auth', auth)
 
 const port = process.env.PORT || 5000
