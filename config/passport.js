d316e44da9aa7f8ccd54a59f77ce8e9bcea4f95e
Matthew Jaikaran <mattjaikaran@gmail.com>
2018-04-05 15:07:21 -0400

    test, cleaned up passportcomments

--- config/passport.js
+++ config/passport.js
@@ -13,8 +13,6 @@ module.exports = (passport) => {
       callbackURL: '/auth/google/callback',
       proxy: true
     }, (accessToken, refreshToken, profile, done) => {
-      // console.log(accessToken)
-      // console.log(profile)
       const image = profile.photos[0].value.substring(0, profile.photos[0].value.indexOf('?'))
 
       const newUser = {
